8ceb0e6a1b1dd8b07e52832f3bdcece5545080f0
Mohsin Habiya <mohsin.salim567@gmail.com>
2023-06-09 20:28:24 +0400

    Update app.js

--- expressJS/app.js
+++ expressJS/app.js
@@ -8,5 +8,17 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// another path /example. Same demo
+app.get('/example', (req, res) => {
+  res.send('hitting example route');
+});
+
+// Setting route params with :
+app.get('/example/:name/:age', (req, res) => {
+  // Console will ask for the params I mentioned in the URL
+  console.log(req.params);
+  res.send('example with route params');
+});
+
 // create the port for the user to access said route
 app.listen(3000);
