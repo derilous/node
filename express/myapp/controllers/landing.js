fe56ff355439687c7b7190ae95d2d376a40ca368
Mohsin Habiya <mohsin.salim567@gmail.com>
2023-06-10 18:50:41 +0400

    handling email content with POST requests

--- express/myapp/controllers/landing.js
+++ express/myapp/controllers/landing.js
@@ -1,5 +1,12 @@
-exports.getLanding = function (req, res, next) {
+const app = require('../app');
+// const bodyParser = require('body-parser');
+
+exports.getLanding = function (req, res) {
   res.render('landing', { title: 'Express' });
 };
-
 // exporting this file to be picked up on index.js in the routes folder
+
+exports.submitLead = function (req, res) {
+  console.log(req.body.leadEmail);
+  res.send('email is ' + req.body.leadEmail);
+};
--- express/myapp/routes/index.js
+++ express/myapp/routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 
 // calling index from controllers folder instead of writing lengthy code
 let landing = require('../controllers/landing');
+const app = require('../app');
 
 router.get('/', landing.getLanding);
-
+router.post('/', landing.submitLead);
 module.exports = router;
